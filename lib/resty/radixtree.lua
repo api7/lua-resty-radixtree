3a6422ba87b890ee6b51fa4e070ef35ed1ca5e23
tzssangglass <tzssangglass@gmail.com>
2022-01-13 14:10:18 +0800

    fix: no opts option should work fine
    Closes #108

--- lib/resty/radixtree.lua
+++ lib/resty/radixtree.lua
@@ -608,13 +608,6 @@ end
 
 
 local function _match_from_routes(routes, path, opts, args)
-    if opts == empty_table then
-        local route = routes[1]
-        if not route or route.method == 0 then
-            return route
-        end
-    end
-
     local opts_matched_exists = (opts.matched ~= nil)
     for _, route in ipairs(routes) do
         if match_route_opts(route, opts, args) then
